b13bf61344c1df79eba5abb343cefea2baa5bbde
Luis montoya <luishm2024@gmail.com>
2021-10-19 11:13:03 -0500

    inicializacion proyecto

---
+++ src/main/java/com/platzi/FundamentoSpring/FundamentoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.FundamentoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/FundamentoSpring/FundamentoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.FundamentoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
